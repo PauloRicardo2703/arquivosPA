916c772a09c6cd2362391fe522da6d998131a332
PauloRicardo2703 <paulinhopr2703@gmail.com>
2024-07-25 12:32:32 -0300

    Add files via upload

---
+++ aula1-revisao.js
@@ -0,0 +1,15 @@
+// Criando Variáves:
+let saudacao= "Olá Mundo"; // Tipoi de dado "string"
+let numero= 25; // Tipo de dado inteiro (int)
+let decimal= 3.14; // Tipo de caso flutuante (float)
+let verdadeiro= true; // Tipo de dado booleano (verdadeiro)
+let falso= false; // Tipo de dado booleano (falso)
+
+// Saídas dos dados atribuídos nas variáveis:
+console.log(saudacao);
+console.log("+++++++++++++++++++++++++++");
+console.log("numero: ", numero);
+console.log("decimal: ", decimal)
+console.log("verdadeiro: ", verdadeiro);
+console.log("falso: ", falso);
+console.log(" ");
